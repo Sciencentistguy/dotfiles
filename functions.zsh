6218e2ec9c7b99e4cd848211bf0af646f1d21b51
Jamie Quigley <jamie@quigley.xyz>
2022-05-26 11:41:17 +0100

    Remove `ex()`
    See https://github.com/Sciencentistguy/nixfiles/commit/877d077d42576f0add502f88af3b58eceb74b2e3

--- functions.zsh
+++ functions.zsh
@@ -29,24 +29,6 @@ ffcompare() {
     ffplay $2
 }
 
-ex() {
-    if [ -f $1 ]; then
-        case $1 in
-        *.tar*) tar xvf $1 ;;
-        *.bz2) bunzip2 $1 ;;
-        *.rar) unrar x $1 ;;
-        *.gz) gunzip $1 ;;
-        *.t*z*) tar xvf $1 ;;
-        *.zip) unzip $1 ;;
-        *.Z) uncompress $1 ;;
-        *.7z) 7z x $1 ;;
-        *) echo "'$1' cannot be extracted via ex()" ;;
-        esac
-    else
-        echo "'$1' is not a valid file"
-    fi
-}
-
 update-music-library() {
     if [ $(hostname) = chronos ]; then
         rsync -Pa --delete ~/Music/beets/ atlas:/nas/plex/Music
