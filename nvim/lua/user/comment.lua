db35230cd2a8a50a3924c454fa6c66903e6fd9ca
Jamie Quigley <jamie@quigley.xyz>
2022-05-30 13:28:45 +0100

    Nvim: fix ctrl-slash commenting on discordia

--- nvim/lua/user/comment.lua
+++ nvim/lua/user/comment.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_keymap("", "", "<plug>NERDCommenterToggle <cr>", { silent = true })
-vim.api.nvim_set_keymap("v", "", "<plug>NERDCommenterToggle", { silent = true })
+vim.api.nvim_set_keymap("", "<c-/>", "<plug>NERDCommenterToggle <cr>", { silent = true })
+vim.api.nvim_set_keymap("v", "<c-/>", "<plug>NERDCommenterToggle", { silent = true })
 
 vim.g.NERDSpaceDelims = true
