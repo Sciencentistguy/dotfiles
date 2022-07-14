f746b0afc733d80ec200ba129c5204821c1e6285
Jamie Quigley <jamie@quigley.xyz>
2022-07-14 12:15:57 +0100

    Nvim: use telescope for `gd`

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -7,7 +7,7 @@ local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 
 vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+vim.api.nvim_set_keymap("n", "gd", ":Telescope lsp_definitions<CR>", opts)
 vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
